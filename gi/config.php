d3e51d79ec36a9995ec43271b68405a6f782e7a1
ThorloRick <thorlorick@gmail.com>
2025-06-03 14:12:49 -0400

    Update config.php
    Deleted security risks

--- gi/config.php
+++ gi/config.php
@@ -1,13 +1,1 @@
-return [
-    'apiKeys' => [
-        'gradeInsight' => 'AIzaSyCvsnZ89gyNNQ5Fk-7bW9kpzbsAYGchGag',
-        'outhouse' => 'AIzaSyBaHjs64DGiaLwms_YlmaSdxD9MNP43jPc',
-        'percentage' => 'AIzaSyBaHjs64DGiaLwms_YlmaSdxD9MNP43jPc',
-    ],
-    'spreadsheetIds' => [
-        'gradeInsight' => '1xw1VU5KRT_lAYYdF62t5oQ3BZm2FowTsQ3AsFxwbEOM',
-        'outhouse' => '1phAIVtUMagoOF37_9TzfS-HAejpVeT3cdTjg0NYj_og',
-        'percentage' => '1uQstsXok6L1pI_dyMYBxJo4Ilo4I66Nz5U6W1Z3vjL8',
-    ],
-];
 
