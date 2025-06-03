d81c702acf0b56186cb52730653711a9265bbde8
ThorloRick <thorlorick@gmail.com>
2025-06-03 14:14:49 -0400

    Update config_variables.php
    deleted security risks

--- config_variables.php
+++ config_variables.php
@@ -1,13 +1,1 @@
-<?php
-// config_variables.php
 
-return [
-    'apiKey' => 'AIzaSyCvsnZ89gyNNQ5Fk-7bW9kpzbsAYGchGag',
-    'spreadsheetIds' => [
-        '1xw1VU5KRT_lAYYdF62t5oQ3BZm2FowTsQ3AsFxwbEOM',
-        
-        
-        
-        // Add more spreadsheet IDs as needed
-    ],
-];
